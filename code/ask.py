df4bf13165f529d5f8e41abe754e9455e898cefc
myhaooo <67223745+myhaooo@users.noreply.github.com>
2024-11-29 17:42:48 +0800

    Add files via upload

--- code/ask.py
+++ code/ask.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 import time
 import json
 
-searchstr="jump"
+searchstr="Test"
 Headless=False
 
 def search(beg,end):
@@ -15,17 +15,20 @@ def search(beg,end):
         x=ask(string,str(i))
         if x==1:
             print("搜索到"+str(i)+"条")
-            break
+            with open('./start.txt','w') as f:
+                f.write(str(i))
+            return
 
 
 def ask(string,uid):
     driver.get(string)
     try:
-        a1 = driver.find_element(By.ID,'status_table').text
         s1=driver.find_element(By.CLASS_NAME,"content").text
+        #print(s1)
         if("提交记录" in s1 and "不正确" in s1):#在搜索至最后一个时末尾自动停止
             return 1
-        time.sleep(0.1)
+        a1 = driver.find_element(By.ID,'status_table').text
+        time.sleep(0.5)
     except:
         return 0
     else:
@@ -35,10 +38,14 @@ def ask(string,uid):
         # if("Accepted" in a1):
         #     f1=driver.find_element(By.TAG_NAME,'code').text
         #     print(f1)
-        if("Accepted" in a1 and (searchstr in a1 or searchstr=='')):#搜索所有AC的代码，若有searchstr则追加搜索
-            print(a1)
+        print(a1)
+        if(("Success" in a1 or 'Accepted' in a1 )and (searchstr in a1 or searchstr=='')):#搜索所有AC的代码，若有searchstr则追加搜索
+            #print(a1)
             f1=driver.find_element(By.TAG_NAME,'code').text
+            if("fft.in" in f1 ):
+                return 0
             with open("../download_code/"+searchstr+"_"+uid+'.txt','w') as f:#存放在./code 文件夹下
+                f.write(a1)
                 f.write(f1)
             time.sleep(3)
         return 0
@@ -58,6 +65,9 @@ with open('./cookies.txt','r') as f:
     for cookie in cookies_list:
         driver.add_cookie(cookie)
 driver.refresh()
-
-
-search(246082,255105)
+#254184
+beginn=0
+with open('./start.txt','r') as f:
+    c = f.read()
+    beginn=int(c)
+search(beginn,355105)
